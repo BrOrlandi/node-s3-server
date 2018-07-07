cb3552bc7b28ead6f48a25da85a525767a3ce74d
BrOrlandi <brorlandi@gmail.com>
2018-07-06 21:42:15 -0300

    Serving routes to S3 files.

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+  params: {
+    Bucket: process.env.AWS_BUCKET,
+  }
+});
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.get('/download/:name', (req, res) => {
+  const fileName = req.params.name;
+  if (!fileName) {
+    return res.status(400).end('missing file name');
+  }
+  var options = {
+    Bucket: process.env.AWS_BUCKET,
+    Key: fileName,
+  };
+
+  console.log('File: ', fileName);
+
+  res.attachment(fileName);
+  s3.getObject(options).createReadStream().pipe(res);
+});
+
+app.listen(3000, () => {
+  console.log('Node Server is listening');
+});
